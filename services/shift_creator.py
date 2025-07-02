849973998126ea8824f1ce292a409a1d6d7f24c8
khalarlay21501 <C250324@ccg.ac.jp>
2025-07-02 17:18:10 +0900

    頭痛い

--- services/shift_creator.py
+++ services/shift_creator.py
@@ -17,7 +17,9 @@ class ShiftCreator:
         try:
             start = datetime.strptime(self.start_date, self.date_format).date()
             end = datetime.strptime(self.end_date, self.date_format).date()
+            print(start,end)
             return start, end
+        
         except ValueError as e:
             print("error", e)
             return None, None
@@ -41,9 +43,10 @@ class ShiftCreator:
             else:
                 is_festival.append(0)
             current += timedelta(days=1)
+        print(is_festival)
         return is_festival
 
-    def weather_data(self):
+    def weather_data(self,start,end):
         cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
         retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
         openmeteo = openmeteo_requests.Client(session=retry_session)
@@ -54,10 +57,10 @@ class ShiftCreator:
             "longitude": self.longitude,
             "daily": ["rain_sum", "snowfall_sum", "weather_code", "temperature_2m_max"],
             "timezone": "Asia/Tokyo",
-            "start_date": self.start_date,  
-            "end_date": self.end_date
+            "start_date": start.strftime("%Y-%m-%d"),
+            "end_date": end.strftime("%Y-%m-%d")
         }
-
+   
         responses = openmeteo.weather_api(url, params=params)
         response = responses[0]
 
@@ -82,15 +85,61 @@ class ShiftCreator:
             }
 
         daily_dataframe = pd.DataFrame(data=daily_data)
+        daily_dataframe["date"] = daily_dataframe["date"].dt.tz_localize(None)
+
         print(daily_dataframe)
         return daily_dataframe
     
-    #def data_from_model(self,start,end,is_ffestival,daily_dataframe):
+    def pred_from_model(self, start, end, is_festival, weather_df):
+        date_range = pd.date_range(start=start,end=end)
+        df = pd.DataFrame({
+            "date" :date_range,
+            "is_festival" : is_festival
+        })
+        df["weekday"] = df["date"].dt.weekday
+        df["month"] = df["date"].dt.month
+        df["day"] = df["date"].dt.day
+        df["year"] = df["date"].dt.isocalendar().year
+        df["weekofyear"] = df["date"].dt.isocalendar().week
         
-    
+        def assign_season(month):
+            if month in [12, 1, 2]:
+                return "winter"
+            elif month in [3, 4, 5]:
+                return "spring"
+            elif month in [6, 7, 8]:
+                 return "summer"
+            else:
+                return "autumn"
+
+        df["season"] = df["month"].apply(assign_season)
+        season_encoder = joblib.load("model/season_encoder.pkl")
+        df["season"] = season_encoder.transform(df["season"])
+        df = df.merge(weather_df, on="date", how="left")
         
+        features = ["weekday", "month", "is_festival", "season",
+                "weather_code", "temperature", "snowfall_sum", "rain_sum",
+                "year", "day", "weekofyear"]
+        
+        model_input = df[features]
+        model = joblib.load("model/sales_model.pkl")
+        preds = model.predict(model_input)
 
+        df["predicted_sales"] = preds
 
+        print(df[["date", "predicted_sales"]])
+        return df[["date", "predicted_sales"]]
+
+     
+    def pred_staff_count(self, sales_preds):
+        model = joblib.load("model/staff.pkl")
+        input_df = sales_preds[["predicted_sales"]].rename(columns={"predicted_sales": "sales"})
+        staff_levels = model.predict(input_df)
+        sales_preds["predicted_staff_level"] = staff_levels
+        print(sales_preds[["date", "predicted_sales", "predicted_staff_level"]])
+        return sales_preds[["date", "predicted_sales", "predicted_staff_level"]]
+
+            
 
 if __name__ == "__main__":
     start_date = input("Plz enter start date (YYYY-MM-DD): ")
@@ -99,14 +148,16 @@ if __name__ == "__main__":
     #saitama
     latitude = 35.8617
     longitude = 139.6455
+    
 
     creator = ShiftCreator(start_date, end_date, latitude, longitude)
     start, end = creator.date_data_from_user()
-
+   
     if start and end:
+        start, end = creator.date_data_from_user()
         festivals = creator.check_festival_range(start, end)
-        weather_df = creator.weather_data()
-        #pred_df = creator.pred_from_model(start, end, festivals, weather_df)
+        weather_df = creator.weather_data(start, end) 
+        pred_df = creator.pred_from_model(start, end, festivals, weather_df)
+        pred_staff = creator.pred_staff_count(pred_df)
 
-       
 
