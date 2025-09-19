14d4c084095b719022c6799a893d6fdd410715c4
khalarlay21501 <C250324@ccg.ac.jp>
2025-09-19 11:17:36 +0900

    loat kya par own

--- flask_back/app.py
+++ flask_back/app.py
@@ -32,7 +32,12 @@ from flask_back.services.staff_manager import StaffManager
 #from services.retrain import reTrain_model
 from datetime import date, timedelta
 import pandas as pd
-app = Flask(__name__, static_folder=__name__, static_folder="flutter_front/predictor_web/build/web", static_url_path="")
+app = Flask(
+    __name__,
+    static_folder="flutter_front/predictor_web/build/web",
+    static_url_path=""
+)
+
 CORS(app,resources={r"/api/*": {"origins": "https://cccproject-production.up.railway.app/"}})
   # Enable CORS to allow requests from frontend (e.g. Flutter)
 
