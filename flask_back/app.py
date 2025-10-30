9950e25e85dc209d55bc878858bf98423c0ee9be
khalarlay21501 <C250324@ccg.ac.jp>
2025-10-31 00:21:50 +0900

    testing 10

--- flask_back/app.py
+++ flask_back/app.py
@@ -457,6 +457,7 @@ def search_staff():
 # ---------------------------------------
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # RenderがPORTを渡す
+    app.run(host="0.0.0.0", port=port)
 
