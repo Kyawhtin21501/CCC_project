97f03bda1406352bfc803229d40d3f4505fedb55
khalarlay21501 <C250324@ccg.ac.jp>
2026-01-09 19:26:17 +0900

    fix error and test deply

--- back_end/app.py
+++ back_end/app.py
@@ -1,17 +1,15 @@
 from flask import Flask
-from  back_end.routes.staff_routes import staff_bp
-from  back_end.routes.shift_pre_routes import shift_pre_bp
-from  back_end.routes.daily_report_route import daily_report_bp
-from  back_end.routes.prediction_routes import pred_sales_bp
-from  back_end.routes.shift_routes import shift_ass_bp
-
+from back_end.routes.staff_routes import staff_bp
+from back_end.routes.shift_pre_routes import shift_pre_bp
+from back_end.routes.daily_report_route import daily_report_bp
+from back_end.routes.prediction_routes import pred_sales_bp
+from back_end.routes.shift_routes import shift_ass_bp
 from flask_cors import CORS
-
-
 
 def create_app():
     application = Flask(__name__)
-    CORS(app)
+    # 修正点1: app ではなく application に変更
+    CORS(application)
 
     application.register_blueprint(staff_bp)
     application.register_blueprint(shift_pre_bp)
@@ -21,9 +19,8 @@ def create_app():
     return application
 
 
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
+    # ローカル実行用
     app.run(host="0.0.0.0", port=5000, debug=True)
-
-  
