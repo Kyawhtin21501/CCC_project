b18b423f63591e82d91170bd01b27e135a9607fa
khalarlay21501 <C250324@ccg.ac.jp>
2026-01-09 12:37:42 +0900

    fix error

--- back_end/app.py
+++ back_end/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from  .routes.staff_routes import staff_bp
-from  .routes.shift_pre_routes import shift_pre_bp
-from  .routes.daily_report_route import daily_report_bp
-from  .routes.prediction_routes import pred_sales_bp
-from  .routes.shift_routes import shift_ass_bp
+from  back_end.routes.staff_routes import staff_bp
+from  back_end.routes.shift_pre_routes import shift_pre_bp
+from  back_end.routes.daily_report_route import daily_report_bp
+from  back_end.routes.prediction_routes import pred_sales_bp
+from  back_end.routes.shift_routes import shift_ass_bp
 
 from flask_cors import CORS
 
