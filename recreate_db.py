5aa883e24e2a9a2ff6580a762176762937bdd665
call-it-is <kyipyarhlaing910@gmail.com>
2026-01-15 23:17:24 +0900

    merge

--- recreate_db.py
+++ recreate_db.py
@@ -8,5 +8,5 @@ from back_end.models.pred_sales_model import Pred_sales
 
 from back_end.models.shift_model import ShiftMain
 #Base.metadata.drop_all(bind=engine)
-#Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 print("DB recreated")
